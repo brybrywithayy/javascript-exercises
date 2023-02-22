6c9166d7f060a1acf3aa56464b78d8e0c7d939fa
Bryan Dalina <brybrywithayy@gmail.com>
2023-02-22 18:03:22 -0500

    Start implementation of sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let start = num1;
+    let end = num2;
 
+    // need to add every number between and including these points
 };
 
 // Do not edit below this line
