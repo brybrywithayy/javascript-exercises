e4016775047eb81fc5febff006da556f96362248
Bryan Dalina <brybrywithayy@gmail.com>
2023-02-22 13:56:09 -0500

    Add changes to repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(str, numRepeat) {
+    let totalStr = "";
 
+    if (numRepeat < 0) {
+        totalStr = "ERROR";
+    } else {
+        for (let i=1; i<=numRepeat; i++) {
+            totalStr += str;
+        }
+    }
+
+    return totalStr;
 };
 
 // Do not edit below this line
