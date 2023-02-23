b8567bcf5a39ba078aa698edc663b94b318f52a8
Bryan Dalina <brybrywithayy@gmail.com>
2023-02-22 20:32:26 -0500

    Complete implementation of leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+/**
+ * What is a leap year?
+ *     Divisible by 4
+ *     NOT divisible by 100
+ *     divisible by 400
+ */
+
+const leapYears = function(year) {
+    
+if ((year % 4 === 0) && (year % 100 != 0 || year % 400 === 0)) {
+        return true;
+    } else {
+        return false;
+    }
 
 };
 
